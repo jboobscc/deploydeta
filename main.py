6aee54d5c18e318f5cab3c9dbdd8e66bdbb88a66
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 15:13:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from flask import Flask, request, jsonify, Response
-import requests
- 
- 
-def create_app():
-    app = Flask(__name__)
-    app.config['JSON_AS_ASCII'] = False
-    return app
- 
- 
-app = create_app()
- 
- 
-@app.before_request
-def proxy():
-    headers = {h[0]: h[1] for h in request.headers}
-    url = request.url
-    headers['x-token'] = '***'
-    # 一些自己的逻辑...
-    return requests.request(request.method, url, data=request.json, headers=headers).content
+from flask import Flask
+from flask_websocket import WebSocket
+
+app = Flask(__name__)
+ws = WebSocket(app)
+
+@ws.on('click')
+def click(data):
+print(data)
