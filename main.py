7510813b021d2df01f2b17cde310d81aa0f16265
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 12:59:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ app = FastAPI()
 async def index():
 	url= "https://raw.githubusercontent.com/Jboobs/deploy/main/LICENSE"
 	response = requests.get(url).text
-	name = os.getenv("NAME", "world")
+	#name = os.getenv("NAME", "world")
+	return response
+
+@app.get("/ip")
+async def index():
+	url= "https://api.ip.sb/ip"
+	response = requests.get(url).text
 	return response
