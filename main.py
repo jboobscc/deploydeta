e4321652d1b9e96723622d7a055b9829c67730ff
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 13:51:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-url = "https://api.ip.sb"
-def index():
-  print(url, '提取完毕')
-  response = url
-  return response
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+def get_html(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+        "Referer": url
+    }
+    response = requests.get(url, headers=headers)
+    return response.text
+
+@app.route('/', methods=["GET"])
+def hello_world():
+  return "Hello World"
