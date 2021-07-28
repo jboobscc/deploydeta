5250f626f78be3dad88b48c8a81981df5f0e9878
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 13:54:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,24 @@ app = Flask(__name__)
 def get_html(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
-        "Referer": url
     }
     response = requests.get(url, headers=headers)
     return response.text
 
 @app.route('/', methods=["GET"])
+
 def hello_world():
   return "Hello World"
+
+@app.route('/ip', methods=["GET"])
+def getip():
+	url= "https://api.ip.sb/ip"
+	response = get_html(url)
+	return response
+
+
+@app.route('/get', methods=["GET"])
+def getip():
+	url= "http://httpbin.org/get"
+	response = get_html(url)
+	return response
