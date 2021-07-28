2f09e62631131fed9adcadc87544fd577c95a9bb
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 13:55:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import requests
 app = Flask(__name__)
 
 def get_html(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
-    }
-    response = requests.get(url, headers=headers)
-    return response.text
+	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"}
+	response = requests.get(url, headers=headers)
+	return response.text
 
 @app.route('/', methods=["GET"])
 def hello_world():
