3b2371b25f27a52b8c6e23a6464d1776b098ff8b
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 12:40:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 async def index():
+	url= "http://ip.sb"
+	response = requests.get(url)
 	name = os.getenv("NAME", "world")
-	return f"hello {name}!"
+	return f"hello {response}!"
