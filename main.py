14e5ddf7f2c3b2ac6486351f73e6ef35aef21d97
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 15:40:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import requests
 app = Flask(__name__)
 
 def get_html(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
-    }
+	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"}
 	response = requests.get(url, headers=headers)
 	return response.text
 
