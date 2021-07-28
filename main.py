12f5eeefceb1da778899fc35203b1c8838e36f7c
Jboobs <87639947+Jboobs@users.noreply.github.com>
2021-07-28 14:52:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from contextlib import closing
 import requests
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-def get_html(url):
-	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"}
-	response = requests.get(url, headers=headers)
-	return response.text
 
-@app.route('/', methods=["GET"])
-def hello_world():
-	return "Hello World"
+@app.before_request
+def before_request():
+    url = request.url
+    method = request.method
+    data = request.data or request.form or None
+    headers = dict()
+    for name, value in request.headers:
+        if not value or name == 'Cache-Control':
+            continue
+        headers[name] = value
 
-@app.route('/ip', methods=["GET"])
-def getip():
-	url= "https://api.ip.sb/ip"
-	response = get_html(url)
-	return response
-
-
-@app.route('/get', methods=["GET"])
-def gethttpbin():
-	url= "http://httpbin.org/get"
-	response = get_html(url)
-	return response
+    with closing(
+        requests.request(method, url, headers=headers, data=data, stream=True)
+    ) as r:
+        resp_headers = []
+        for name, value in r.headers.items():
+            if name.lower() in ('content-length', 'connection',
+                                'content-encoding'):
+                continue
+            resp_headers.append((name, value))
+        return Response(r, status=r.status_code, headers=resp_headers)
